f4011092dedd2b44bf773bb8409ae87d64e8994b
Kemar Norton <kemarnorton@outlook.com>
2022-05-24 19:27:34 -0500

    Add Logging with Serilog and Seq for CartItem Controller.

--- eCommerce.API/Controllers/CartItemsController.cs
+++ eCommerce.API/Controllers/CartItemsController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using eCommerce.API.Date;
 using eCommerce.API.Models_DTOs.CartItem;
 using AutoMapper;
+using eCommerce.API.Static_ErrorMessage;
 
 namespace eCommerce.API.Controllers
 {
@@ -17,43 +18,64 @@ namespace eCommerce.API.Controllers
     {
         private readonly eCommerceDBContext _context;
         private readonly IMapper mapper;
+        private readonly ILogger<CartItemsController> logger;
 
-        public CartItemsController(eCommerceDBContext context, IMapper mapper)
+        public CartItemsController(eCommerceDBContext context, IMapper mapper, ILogger<CartItemsController> logger)
         {
             _context = context;
             this.mapper = mapper;
+            this.logger = logger;
         }
 
         // GET: api/CartItems
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CartItemReadOnlyDto>>> GetCartItems()
         {
-          if (_context.CartItems == null)
-          {
-              return NotFound();
-          }
-            var cartItems = await _context.CartItems.ToListAsync();
-            var cartItemDtos = mapper.Map<IEnumerable<CartItemReadOnlyDto>>(cartItems);
-            return Ok(cartItemDtos);
+            try
+            {
+                if (_context.CartItems == null)
+                {
+                    return NotFound();
+                }
+
+                var cartItems = await _context.CartItems.ToListAsync();
+                var cartItemDtos = mapper.Map<IEnumerable<CartItemReadOnlyDto>>(cartItems);
+                return Ok(cartItemDtos);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Error Perfoming GET in {nameof(GetCartItems)}");
+                return StatusCode(500, Messages.Error500Message);
+            }
         }
 
         // GET: api/CartItems/5
         [HttpGet("{id}")]
         public async Task<ActionResult<CartItemReadOnlyDto>> GetCartItem(int id)
         {
-          if (_context.CartItems == null)
-          {
-              return NotFound();
-          }
-            var cartItem = await _context.CartItems.FindAsync(id);
-
-            if (cartItem == null)
+            throw new Exception("Testing Error log");
+            try
             {
-                return NotFound();
-            }
+                if (_context.CartItems == null)
+                {
+                    return NotFound();
+                }
+                var cartItem = await _context.CartItems.FindAsync(id);
 
-            var cartItemDto = mapper.Map<CartItemReadOnlyDto>(cartItem);
-            return Ok(cartItemDto);
+                if (cartItem == null)
+                {
+                    logger.LogWarning($"Record Not Found: {nameof(GetCartItem)} - ID: {id}");
+                    return NotFound();
+                }
+
+                var cartItemDto = mapper.Map<CartItemReadOnlyDto>(cartItem);
+                return Ok(cartItemDto);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Error Perfoming GET in {nameof(GetCartItems)}");
+                return StatusCode(500, Messages.Error500Message);
+            } 
         }
 
         // PUT: api/CartItems/5
@@ -63,6 +85,7 @@ namespace eCommerce.API.Controllers
         {
             if (id != cartItemDto.Id)
             {
+                logger.LogWarning($"Update ID Invalid {nameof(PutCartItem)} - ID: {id}");
                 return BadRequest();
             }
 
@@ -70,6 +93,7 @@ namespace eCommerce.API.Controllers
 
             if (cartItem == null)
             {
+                logger.LogWarning($"{nameof(cartItem)} Record Not Found in {nameof(PutCartItem)} - ID: {id}");
                 return NotFound();
             }
 
@@ -80,7 +104,7 @@ namespace eCommerce.API.Controllers
             {
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException)
+            catch (DbUpdateConcurrencyException ex)
             {
                 if (!await CartItemExists(id))
                 {
@@ -88,7 +112,8 @@ namespace eCommerce.API.Controllers
                 }
                 else
                 {
-                    throw;
+                    logger.LogError(ex, $"Error Perfoming GET in {nameof(PutCartItem)}");
+                    return StatusCode(500, Messages.Error500Message);
                 }
             }
 
@@ -100,35 +125,54 @@ namespace eCommerce.API.Controllers
         [HttpPost]
         public async Task<ActionResult<CartItemCreateDto>> PostCartItem(CartItemCreateDto CartItemDto)
         {
-          if (_context.CartItems == null)
-          {
-              return Problem("Entity set 'eCommerceDBContext.CartItems'  is null.");
-          }
-            var cartItem = mapper.Map<CartItem>(CartItemDto);
-            await _context.CartItems.AddAsync(cartItem);
-            await _context.SaveChangesAsync();
+            try
+            { 
+              if (_context.CartItems == null)
+              {
+                  return Problem("Entity set 'eCommerceDBContext.CartItems'  is null.");
+              }
+                var cartItem = mapper.Map<CartItem>(CartItemDto);
+                await _context.CartItems.AddAsync(cartItem);
+                await _context.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetCartItem), new { id = cartItem.Id }, cartItem);
+            }
+            catch (Exception ex)
+            {
+
+                logger.LogError(ex, $"Error Perfoming POST in {nameof(PostCartItem)}", CartItemDto);
+                return StatusCode(500, Messages.Error500Message);
+            }
+         
         }
 
         // DELETE: api/CartItems/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteCartItem(int id)
         {
-            if (_context.CartItems == null)
+            try
             {
-                return NotFound();
+                if (_context.CartItems == null)
+                {
+                    return NotFound();
+                }
+                var cartItem = await _context.CartItems.FindAsync(id);
+                if (cartItem == null)
+                {
+                    logger.LogWarning($"{nameof(cartItem)} Record Not Found In {nameof(DeleteCartItem)} - ID: {id}");
+                    return NotFound();
+                }
+
+                _context.CartItems.Remove(cartItem);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
             }
-            var cartItem = await _context.CartItems.FindAsync(id);
-            if (cartItem == null)
+            catch (Exception ex)
             {
-                return NotFound();
+                logger.LogError(ex, $"Error Performing Delete in {nameof(DeleteCartItem)} - ID: {id}");
+                return StatusCode(500, Messages.Error500Message);
             }
-
-            _context.CartItems.Remove(cartItem);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
         }
 
         private async Task<bool> CartItemExists(int id)
---
+++ eCommerce.API/Static ErrorMessage/Messages.cs
@@ -0,0 +1,7 @@
+﻿namespace eCommerce.API.Static_ErrorMessage
+{
+    public static class Messages
+    {
+        public const string Error500Message = "There was an error completing your request. Please Try Again Later.";
+    }
+}
